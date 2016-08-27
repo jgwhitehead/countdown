ba5e9587d335f1b27352ebc1c767789a17a51d72
jgwhitehead <acejames@msn.com>
2016-08-27 20:47:07 +0100

    added longest word length and refactored

--- WordSolver.py
+++ WordSolver.py
@@ -1,38 +1,34 @@
-import copy
-
 
 def validate_word(word, letters):
     letters_copy = letters[:]
-    # word = 'sulk'
-
-    # print(word)
     for c in word:
         if not c in letters_copy:
-            # print(c)
             return False
         letters_copy.remove(c)
-    # print(word)
     return True
 
 
-file = '/usr/share/dict/words'
-while True:
-    inputted = input('enter 8 letters:')
-    print("{s} letters entered".format(s=len(inputted)))
-    if len(inputted) == 8:
-        letters = []
-        letters.extend(inputted)
-        break
+def get_user_input():
+    letters = []
+    while True:
+        inputted = input('enter 8 or 9 letters:')
+        print("{s} letters entered".format(s=len(inputted)))
+        if len(inputted) == 8 or len(inputted) == 9:
+            letters = []
+            letters.extend(inputted)
+            break
+    return letters
+
 
 matches = []
-
+file = '/usr/share/dict/words'
+letters = get_user_input()
 for word in open(file):
     word = word.lower().strip()
-    # print(word)
     if validate_word(word, letters):
-        # print(word)
         matches.append(word)
 print("{s} matches".format(s=len(matches)))
 matches.sort(key=len)
 for match in matches:
     print(match)
+print("longest word length: {longestLength}".format(longestLength=len(matches[-1])))
